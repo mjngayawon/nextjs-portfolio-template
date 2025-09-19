1fadd449099953c5341450d72c88fd977fb44989
MICHAEL JOHN P. NGAYAWON <2222574@slu.edu.ph>
2025-09-19 16:37:39 +0800

    Size change in recent projects

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -11,14 +11,14 @@ const RecentProjects = () => {
         <span className="text-purple">recent projects</span>
       </h1>
 
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div
             key={id}
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            className="lg:min-h-[32.5rem] flex items-center justify-center sm:h-[41rem] sm:w-[570px] w-[80vw] h-[32rem]"
           >
             <PinContainer title={link} href={link}>
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+              <div className="relative flex items-center justify-center sm:w-[570px] sm:h-[40vh] w-[80vw] overflow-hidden h-[30vh] mb-10">
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <img src="/bg.png" alt="bg-img" />
                 </div>
